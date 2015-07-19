3da682c389a98c4967afd1712c867fa24919932f
Tom Enden <tome@wix.com>
2015-07-19 16:00:42 +0300

    added concat task to grunt which concatenates all js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         handlebars: {
             compile: {
                 options: {
                     namespace: "templates",
-                    processName: function(filePath) {
+                    processName: function (filePath) {
                         return filePath.replace(/partials\//, '').replace(/\.hbs$/, '');
                     }
                 },
@@ -13,8 +13,21 @@ module.exports = function(grunt) {
                     "partials/templates.js": "partials/*.hbs"
                 }
             }
+        },
+        concat: {
+            dist: {
+                src: [
+                    'js/handlebars-v3.0.3.js', "js/handlebarsHelpers.js",
+                    "partials/templates.js",
+                    "js/modulesObject.js",
+                    "js/modules/*.js",
+                    "js/app.js"
+                ],
+                dest: "dist/built.js"
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-handlebars')
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-concat')
 };
